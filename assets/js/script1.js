840b7d5430ee22728f83c654b03ea609933dedc1
Waqas Hayder <waqashayder01@gmail.com>
2023-06-12 19:55:29 +0500

    final form sobia

---
+++ assets/js/script1.js
@@ -0,0 +1,100 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+///////////////////Cart///////////////////
+jQuery(document).ready(function ($) {
+  //if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+  var $L = 1200,
+    $menu_navigation = $("#main-nav"),
+    $cart_trigger = $("#cd-cart-trigger"),
+    $hamburger_icon = $("#cd-hamburger-menu"),
+    $lateral_cart = $("#cd-cart"),
+    $shadow_layer = $("#cd-shadow-layer");
+
+  //open lateral menu on mobile
+  $hamburger_icon.on("click", function (event) {
+    event.preventDefault();
+    //close cart panel (if it's open)
+    $lateral_cart.removeClass("speed-in");
+    toggle_panel_visibility($menu_navigation, $shadow_layer, $("body"));
+  });
+
+  //open cart
+  $cart_trigger.on("click", function (event) {
+    event.preventDefault();
+    //close lateral menu (if it's open)
+    $menu_navigation.removeClass("speed-in");
+    toggle_panel_visibility($lateral_cart, $shadow_layer, $("body"));
+  });
+
+  //close lateral cart or lateral menu
+  $shadow_layer.on("click", function () {
+    $lateral_cart.removeClass("speed-in");
+    $menu_navigation.removeClass("speed-in");
+    $shadow_layer.removeClass("is-visible");
+    $("body").removeClass("overflow-hidden");
+  });
+
+  //move #main-navigation inside header on laptop
+  //insert #main-navigation after header on mobile
+  move_navigation($menu_navigation, $L);
+  $(window).on("resize", function () {
+    move_navigation($menu_navigation, $L);
+
+    if ($(window).width() >= $L && $menu_navigation.hasClass("speed-in")) {
+      $menu_navigation.removeClass("speed-in");
+      $shadow_layer.removeClass("is-visible");
+      $("body").removeClass("overflow-hidden");
+    }
+  });
+});
+
+function toggle_panel_visibility($lateral_panel, $background_layer, $body) {
+  if ($lateral_panel.hasClass("speed-in")) {
+    $lateral_panel.removeClass("speed-in");
+    $background_layer.removeClass("is-visible");
+    $body.removeClass("overflow-hidden");
+  } else {
+    $lateral_panel.addClass("speed-in");
+    $background_layer.addClass("is-visible");
+    $body.addClass("overflow-hidden");
+  }
+}
+
+function move_navigation($navigation, $MQ) {
+  if ($(window).width() >= $MQ) {
+    $navigation.detach();
+    $navigation.appendTo("header");
+  } else {
+    $navigation.detach();
+    $navigation.insertAfter("header");
+  }
+}
+/////////////PlusMinus Button///////////////////
